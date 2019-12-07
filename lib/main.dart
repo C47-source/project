0b0835f6e128df2a8445d18b66ebb47cbac3be58
イルハム・モーラナ <33782989+C47-source@users.noreply.github.com>
2019-12-08 00:05:38 +0700

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:cabang/splashscreen_view.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'splash Screen',
+    home: SplashScreenPage(),
+  ));
+}
